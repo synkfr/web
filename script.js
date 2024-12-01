be88691c7975a5e2cef840b3437d67285ff686ac
AyoSynk <161512257+synkfr@users.noreply.github.com>
2024-12-01 11:42:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Header Styling */
+header {
+    background: rgba(0, 0, 0, 0.7);
+    color: #fff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    position: sticky;
+    top: 0;
+    z-index: 1000;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s ease-in-out;
+    
+}
+
